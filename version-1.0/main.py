fd47277f287f6c9d1b7f41acdc8550cc19a80317
Venuk12 <118964360+Venuk12@users.noreply.github.com>
2023-05-25 13:19:41 +0200

    Update main.py

--- version-1.0/main.py
+++ version-1.0/main.py
@@ -1,29 +1,34 @@
 from functions import *
 
 
+
 def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
     print("2. All quotes")
     print("3. Exit")
-    add_quote(quotes,filename)
+    print("4.add_quotes")
 
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         filename = "quotes.txt"
+        
         menu()
 
-        choice = input("Choose your an action (1-3): ")
+        choice = input("Choose your an action (1-4): ")
 
         if choice == "1":
             print_quote(random_quote(quotes))
         elif choice == "2":
             view_quotes(quotes)
+        elif choice == "4":
+            add_quote(quotes,filename)
         elif choice == "3":
             print("Good bye...")
             break
+
         else:
             print("Invalid input")
 
